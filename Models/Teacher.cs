68c7042f0ac551ef629030a3f24dd356c5b38489
Programsko Inzenjerstvo <pi_foi@outlook.com>
2022-05-16 20:36:20 +0200

    Implementirali Teacher

--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -9,8 +9,12 @@ namespace Evaluation_Manager.Models
     public class Teacher : Person
     {
         public string Username { get; set; }
-        public string Password { get; set; }    
+        public string Password { get; set; }
 
+        public bool CheckPassword(string password)
+        {
+            return Password == password;
+        }
 
     }
 }
